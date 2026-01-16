3cd2a9e4d3601507eb7552a47a9c7aebc1dccf59
chamithdewmin <chamithsamarakon@gmail.com>
2026-01-16 23:57:06 +0530

    change scroll bar color

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -385,7 +385,7 @@ const Sidebar = ({ isOpen, onClose }) => {
           </div>
 
           {/* Navigation */}
-          <nav className="flex-1 p-4 space-y-1 overflow-y-auto">
+          <nav className="flex-1 p-4 space-y-1 overflow-y-auto sidebar-scroll">
             {menuItems.map((item, idx) => (
               <MenuItem key={idx} item={item} onClose={onClose} />
             ))}
